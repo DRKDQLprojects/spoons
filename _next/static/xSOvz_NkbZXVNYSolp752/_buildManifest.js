9e0419bd6e7b044750afcc5cd4c8e9036a7435f7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-06-28 11:24:00 +0000

    Deploy ddaquel/spoons to github.com/ddaquel/spoons.git:gh-pages

---
+++ _next/static/xSOvz_NkbZXVNYSolp752/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e0fa0bea529a06332c03.css","static/chunks/pages/index-7be5db7beff0cdbf9af6.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
