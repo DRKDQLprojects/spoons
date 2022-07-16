46c2bc6d232339eb27291a0e3d8a401eb91ebdb5
ddaquel <ddaquel@users.noreply.github.com>
2022-07-16 00:55:01 +0000

    Deploying to gh-pages from @ DRKDQLprojects/spoons@1ec146b79e524524e8807c3608596e11f8062cf8 🚀

--- _next/static/ozquvlJWZKSgxiwWfndj1/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e0fa0bea529a06332c03.css","static/chunks/pages/index-e5e1d4d5872d7dcd74c3.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
